2c7cf2a1621c962df967823cbc42814cec80e142
aftabalam507 <aftabalam1996jan@gmail.com>
2024-04-23 17:12:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <main>
+        <Navbar />
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="bg-lightBlue">Navbar</div>
+    </>
+  );
+};
+
+export default Navbar;
